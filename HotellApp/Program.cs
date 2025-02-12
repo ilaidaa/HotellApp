96a581cc94984e37b7fc200918efc1bd40977bc9
ilaidaa <ilaida.e@hotmail.com>
2025-02-12 21:49:28 +0100

    Första utkast för menyn

--- HotellApp/Program.cs
+++ HotellApp/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Skapa meny 
+            Console.WriteLine("======================================");
+            Console.WriteLine("|\t                             |");
+            Console.WriteLine("|\tHANTERA RUM                  |");
+            Console.WriteLine("|\t1. Lägg till nytt rum        |");
+            Console.WriteLine("|\t2. Visa alla rum             |");
+            Console.WriteLine("|\t3. Ändra rumsuppgifter       |");
+            Console.WriteLine("|\t4. Ta bort rum               |");
+            Console.WriteLine("|\t                             |");
+            Console.WriteLine("|\tHANTERA KUNDER               |");
+            Console.WriteLine("|\t5. Lägg till ny kund         |");
+            Console.WriteLine("|\t6. Visa alla kunder          |");
+            Console.WriteLine("|\t7. Ändra kunduppgifter       |");
+            Console.WriteLine("|\t8. Ta bort kund              |");
+            Console.WriteLine("|\t                             |");
+            Console.WriteLine("|\tHANTERA BOKNINGAR            |");
+            Console.WriteLine("|\t9. Skapa ny bokning          |");
+            Console.WriteLine("|\t10. Visa alla bokningar      |");
+            Console.WriteLine("|\t11. Ändra bokning            |");
+            Console.WriteLine("|\t12. Avboka en bokning        |");
+            Console.WriteLine("|\t                             |");
+            Console.WriteLine("|\tTILLGÄNGLIGHET               |");
+            Console.WriteLine("|\t13. Sök lediga rum           |");
+            Console.WriteLine("|\t14. Rumsstatus               |");
+            Console.WriteLine("|\t15. Avsluta                  |");
+            Console.WriteLine("|\t                             |");
+            Console.WriteLine("======================================");
+
+            Console.ReadKey();
+
+
+
+
+
+
         }
     }
 }
