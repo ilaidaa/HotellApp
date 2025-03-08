7c84ee1f1bd8d63665ea7df46086e28afc593551
ilaidaa <ilaida.e@hotmail.com>
2025-03-08 15:34:02 +0100

    Lagt till Context mappen och lagt in mina 3 klasser som tabeller för ett första steg till databas.

---
+++ HotellApp/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotellApp.Context
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Classes.Customer> Customers { get; set; }
+        public DbSet<Classes.Room> Rooms { get; set; } 
+        public DbSet<Classes.Booking> Bookings { get; set; }
+    }
+}
