0f57a0a44fb8ca81034a585e8e67e2df027fa760
ilaidaa <ilaida.e@hotmail.com>
2025-03-09 15:24:52 +0100

    Lagt till 2 konstruktor och 1 metod i ApplicationDbContext()

--- HotellApp/Context/ApplicationDbContext.cs
+++ HotellApp/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,43 @@ namespace HotellApp.Context
         public DbSet<Classes.Customer> Customers { get; set; }
         public DbSet<Classes.Room> Rooms { get; set; } 
         public DbSet<Classes.Booking> Bookings { get; set; }
+
+
+
+
+
+        // 2 konstruktor och en metod ska läggas till nedan. Allt detta för koppla till databas
+        // Konstruktor 1 --> Migration
+        public ApplicationDbContext()
+        {
+
+        }
+
+
+        // Konstruktor 2 --> Options
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        // Metoden som heter OnConfiguring
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
     }
+
+
+
+ 
 }
